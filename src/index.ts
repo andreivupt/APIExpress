738cd7a8705d823e850edb32bae04786e073e082
Andrei Doberstein <andrei.vupt@gmail.com>
2023-07-11 21:24:18 -0300

    feat: server express running

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Gremio vai sair campe');
+import express from 'express';
+import { config } from 'dotenv';
+
+config();
+
+const app = express();
+
+const port = process.env.PORT || 3333;
+
+app.get('/gremiovaisaircampeao', (request, response) => {
+    response.send('Atenção terraquios, o gremio vai sair campeao! E quem é de é, é! Quem não é deixou de é!')
+})
+
+app.listen(port, () => console.log(`listening on port ${port}!`));
+
+
